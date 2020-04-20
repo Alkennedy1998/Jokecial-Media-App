f3cbe39f54ff8bb83b82dc83eec907e2e66b83c7
Alexander Kennedy <rexer1998@gmail.com>
2020-04-20 14:26:50 -0700

    Added login route

--- jokecial-funcitons/functions/index.js
+++ jokecial-funcitons/functions/index.js
@@ -17,12 +17,11 @@ var firebaseConfig = {
 
 const firebase = require('firebase');
 firebase.initializeApp(firebaseConfig);
-
+const db = admin.firestore()
 
 //Get all jokes route
 app.get('/jokes',(req,res)=>{
-    admin
-    .firestore()
+    db
     .collection('jokes')
     .orderBy('createdAt','desc')
     .get()
@@ -52,7 +51,7 @@ app.get('/jokes',(req,res)=>{
          createdAt:new Date().toISOString()
      };
 
-     admin.firestore()
+     db
      .collection('jokes')
      .add(newJoke)
      .then(doc=>{
@@ -64,6 +63,17 @@ app.get('/jokes',(req,res)=>{
      })
  })
 
+ const isEmpty = (string)=>{
+     if(string.trim()==='') return true;
+     else return false
+ }
+ //Check if submitted email is valid
+ const isEmail = (email)=>{
+    const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(email.match(regEx)) return true;
+    else return false;
+ }
+
 //Signup route
 app.post('/signup',(req,res)=>{
     const newUser={
@@ -74,16 +84,88 @@ app.post('/signup',(req,res)=>{
 
     };
 
+    let errors = {}
+    if(isEmpty(newUser.email)){
+        errors.email = 'Must not be empty'
+    }else if(!isEmail(newUser.email)){
+        errors.email = 'Must be a valid email'
+    }
+
+    if(isEmpty(newUser.password)) errors.password = "Must not be empty"
+    if(newUser.password!=newUser.confirmPassword) errors.confirmPassword = "Passwords must match"
+    if(isEmpty(newUser.handle)) errors.handle = "Must not be empty"
+
+    if(Object.keys(errors).length>0) return res.status(400).json(errors)
+
     //Validate data
-    firebase.auth().createUserWithEmailAndPassword(newUser.email,newUser.password)
-    .then(data=>{
-        return res.status(201).json({message:`user ${data.user.uid} signed up successfully`});
+    let token,userId;
+    db.doc(`/users/${newUser.handle}`).get()
+    .then(doc=>{
+        if(doc.exists){
+            return(res.status(400).json({handle: 'This handle is already taken'}));
+        } else{
+            return firebase
+            .auth()
+            .createUserWithEmailAndPassword(newUser.email,newUser.password);
+        }
+    })
+    .then(data =>{
+        userId = data.user.uid;
+        return data.user.getIdToken();
+    })
+    .then((idToken)=>{
+        token=idToken;
+        const userCredentials={
+            handle:newUser.handle,
+            email: newUser.email,
+            createdAt: new Date().toISOString(),
+            userId
+        };
+        return db.doc(`/users/${newUser.handle}`).set(userCredentials);
+    })
+    .then(()=>{
+        return res.status(201).json({token})
     })
     .catch(err=>{
         console.error(err);
-        return res.status(500).json({error: err.code});
+        if(err.code === 'auth/email-already-in-use'){
+            return res.status(400).json({email:'Email already in use'})
+        }
+        else{
+            return(res.status(500).json({error:err.code}))
+        }
     })
-})
 
+
+
+
+});
+
+app.post('/login',(req,res)=>{
+    const user={
+        email:req.body.email,
+        password: req.body.password
+    }
+
+    let errors = {};
+
+    if(isEmpty(user.email)) errors.email = 'Must not be empty'    
+    if(isEmpty(user.password)) errors.password = 'Must not be empty'    
+
+    if(Object.keys(errors).length > 0 ) return res.status(400).json(errors)
+    
+
+    firebase.auth().signInWithEmailAndPassword(user.email,user.password)
+    .then(data=>{
+        return data.getIdToken()
+    })
+    .then(token=>{
+        return res.json({token})
+    })
+    .catch(err=>{
+        console.error(err);
+        return res.status(500).json({error: err.code})
+    });
+});
 
  exports.api = functions.https.onRequest(app);
